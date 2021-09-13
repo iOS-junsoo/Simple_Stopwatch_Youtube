ab210476e9a222b356a047193008e517c25cadd0
kimjunsoo <rlawnstn01023@naver.com>
2021-09-13 10:24:35 +0900

    Initial Commit

---
+++ Simple_Timer_Youtube/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Simple_Timer_Youtube
+//
+//  Created by 준수김 on 2021/09/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
